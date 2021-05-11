6fb8d6d14da91a525baf5f09c42a88e8c01bcb96
abdoukuolem96 <abdoukuolem96@gmail.com>
2021-05-11 06:44:08 +0100

    Request permission the first time Removed getLocation fucntion Use mLocationOverlay.getMyLocation to get current position

--- app/src/main/java/com/esi/navigator_22/MainActivity.java
+++ app/src/main/java/com/esi/navigator_22/MainActivity.java
@@ -1,32 +1,34 @@
 package com.esi.navigator_22;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.location.Address;
-import android.location.Criteria;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.osmdroid.bonuspack.routing.GraphHopperRoadManager;
 import org.osmdroid.bonuspack.routing.OSRMRoadManager;
 import org.osmdroid.bonuspack.routing.Road;
 import org.osmdroid.bonuspack.routing.RoadManager;
@@ -44,8 +46,6 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import java.util.concurrent.Executors;
 
 import okhttp3.Call;
@@ -69,23 +69,28 @@ public class MainActivity extends AppCompatActivity {
     ImageView currentPosition;
 
     Station a = new Station();
-    GeoPoint currentLocation = new GeoPoint(35.2023025901554, -0.6302970012564838);
+    GeoPoint defaultPosition = new GeoPoint(35.2023025901554, -0.6302970012564838);
+    GeoPoint currentLocation = new GeoPoint(0.0, 0.0);
     GeoPoint point = new GeoPoint(0.0, 0.0);
 
     DbHelper database = new DbHelper(this);
 
     ArrayList<Station> stations = new ArrayList<>();
     ArrayList<GeoPoint> chemin = new ArrayList<>();
+    public ArrayList<GeoPoint> history = new ArrayList<>();
     int minZ = 2;
     int maxZ = 17;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        OkHttpClient client = new OkHttpClient();
         myMap = findViewById(R.id.map);
+        currentPosition = findViewById(R.id.currentPosition);
+        myMap.getController().setCenter(new GeoPoint(35.2023025901554, -0.6302970012564838));
 
         Runnable downloadMapToCache = () -> runOnUiThread(() -> {
             myMap.setTileSource(TileSourceFactory.HIKEBIKEMAP);
@@ -95,13 +100,24 @@ public class MainActivity extends AppCompatActivity {
         });
         Executors.newSingleThreadExecutor().execute(downloadMapToCache);
 
-        currentPosition = findViewById(R.id.currentPosition);
-
-        OkHttpClient client = new OkHttpClient();
 
         myMap.getController().setZoom(15.0);
-        getLocation();
-        myMap.getController().setCenter(currentLocation);
+
+        if ((ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED)) {
+            Log.d("LogGps", "Permissions granted");
+        } else {
+            // You can directly ask for the permission.
+            Toast.makeText(this, "Localisation requise", Toast.LENGTH_LONG);
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1
+            );
+//            getLocation();
+            Log.d("LogGps", "Permission check");
+        }
+
 
         mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getApplicationContext()), this.myMap);
         Drawable currentDraw = ResourcesCompat.getDrawable(getResources(), R.drawable.person, null);
@@ -109,8 +125,12 @@ public class MainActivity extends AppCompatActivity {
         if (currentDraw != null) {
             currentIcon = ((BitmapDrawable) currentDraw).getBitmap();
         }
-        mLocationOverlay.enableMyLocation();
         mLocationOverlay.setDirectionArrow(currentIcon, currentIcon);
+        mLocationOverlay.enableMyLocation();
+        mLocationOverlay.getMyLocation();
+//        history.add(mLocationOverlay.getMyLocation());
+
+
         echelle = new ScaleBarOverlay(this.myMap);
         myMap.getOverlays().add(echelle);
         myMap.setMultiTouchControls(true);
@@ -121,9 +141,27 @@ public class MainActivity extends AppCompatActivity {
         myMap.getOverlays().add(mLocationOverlay);
 
         currentPosition.setOnClickListener(v -> {
-            getLocation();
+//            currentLocation = history.get(history.size()-1);
+//            locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+//            Criteria criteres = new Criteria();
+//            criteres.setAccuracy(Criteria.ACCURACY_FINE);
+//            criteres.setAltitudeRequired(true);
+//            criteres.setBearingRequired(true);
+//            criteres.setSpeedRequired(true);
+//            criteres.setCostAllowed(true);
+//            criteres.setPowerRequirement(Criteria.POWER_MEDIUM);
+//            criteres.setPowerRequirement(Criteria.POWER_MEDIUM);
+//            fournisseur = locationManager.getBestProvider(criteres, true);
+//            Location l = new Location(GPS_PROVIDER);
+////            ecouteurGPS.onLocationChanged(locationManager.getLastKnownLocation(fournisseur));
+//            ecouteurGPS.onLocationChanged(locationManager.getLastKnownLocation(GPS_PROVIDER));
+//            currentLocation = history.get(history.size());
+
+            currentLocation = mLocationOverlay.getMyLocation();
+            Log.d("LogGps", "Button clicked and history = " + history.size());
             myMap.getController().setCenter(currentLocation);
-            Log.d("Offline", "Button clicked");
+            myMap.getController().setZoom(16.0);
+            Log.d("LogGps", "Button clicked and current location = " + currentLocation.toString());
         });
 
         Request request = new Request.Builder()
@@ -165,19 +203,100 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(policy);
 
         stations = database.getAllStations();
-        Log.d("databaseTest", String.valueOf(stations.size()));
+        Log.d("LogDatabase", String.valueOf(stations.size()));
         for (int i = 0; i < stations.size(); i++) {
             addMarker(this, myMap, stations.get(i).coordonnees, stations.get(i).nomFr, stations.get(i).nomAr);
         }
 
         chemin = database.getAllPointsChemin();
-        Log.d("databaseTest", String.valueOf(chemin.size()));
+        Log.d("LogDatabase", String.valueOf(chemin.size()));
         tracerChemin(chemin, myMap);
 
 
-        Log.d("databaseTest", database.getAllPointsChemin().toString());
+        Log.d("LogDatabase", database.getAllPointsChemin().toString());
 
 
+    }
+
+    LocationListener ecouteurGPS = new LocationListener() {
+
+
+        @Override
+        public void onLocationChanged(Location localisation) {
+//            String coordonnees = String.format("Latitude : %f - Longitude : %f\n", localisation.getLatitude(), localisation.getLongitude());
+            GeoPoint current = new GeoPoint(0.0, 0.0);
+//            current = new MyLocationNewOverlay(new GpsMyLocationProvider(getApplicationContext()), myMap).getMyLocation();
+            current = mLocationOverlay.getMyLocation();
+            if (localisation != null) {
+                history.add(new GeoPoint(localisation.getLatitude(), localisation.getLongitude()));
+            }
+            Log.d("LogGps", "Marche et currentlocation1 = " + String.valueOf(history.size()));
+            Log.d("LogGps", "Marche et currentlocation2 = " + String.valueOf(current));
+            myMap.getController().setCenter(current);
+            myMap.getController().setZoom(16.0);
+            Log.d("LogGps", "onLocationChanged-252");
+            myMap.invalidate();
+
+//            List<Address> adresses = null;
+//            try {
+////                adresses = geocoder.getFromLocation(localisation.getLatitude(), localisation.getLongitude(), 1);
+//            } catch (IllegalArgumentException illegalArgumentException) {
+//                Log.e("GpsLog", "erreur " + coordonnees, illegalArgumentException);
+//            }
+//
+//            if (adresses == null || adresses.size() == 0) {
+//                Log.e("GpsLog", "erreur aucune adresse !");
+//            } else {
+//                Address adresse = adresses.get(0);
+//                ArrayList<String> addressFragments = new ArrayList<>();
+//
+//                for (int i = 0; i <= adresse.getMaxAddressLineIndex(); i++) {
+//                    addressFragments.add(adresse.getAddressLine(i));
+//                }
+//            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+//            getLocation();
+            Log.d("LogGps", "Gps status changed");
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+            Log.d("LogGps", "Gps disabled");
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+            Log.d("LogGps", "Gps enabled");
+//            getLocation();
+        }
+    };
+
+
+    protected void onDestroy() {
+        super.onDestroy();
+        arreterLocalisation();
+    }
+
+    public void onResume() {
+        super.onResume();
+        mLocationOverlay.enableMyLocation();
+//        getLocation();
+    }
+
+    public void onPause() {
+        super.onPause();
+        mLocationOverlay.disableMyLocation();
+//        getLocation();
+    }
+
+    private void arreterLocalisation() {
+        if (locationManager != null) {
+            locationManager.removeUpdates(ecouteurGPS);
+            ecouteurGPS = null;
+        }
     }
 
     private void fetchAllStations(Response response) throws IOException {
@@ -257,6 +376,7 @@ public class MainActivity extends AppCompatActivity {
         mapView.getOverlayManager().add(line);
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     public void addMarker(Context context, MapView map, GeoPoint position, String nomStation, String nomAr) {
         float[] distance = new float[1];
         Location.distanceBetween(currentLocation.getLatitude(), currentLocation.getLongitude(), position.getLatitude(), position.getLongitude(), distance);
@@ -273,20 +393,21 @@ public class MainActivity extends AppCompatActivity {
         marker.setOnMarkerClickListener((marker1, map1) -> {
             map1.invalidate();
             ArrayList<GeoPoint> route = new ArrayList<>();
-            route.add(currentLocation);
+            route.add(mLocationOverlay.getMyLocation());
             route.add(new GeoPoint(marker1.getPosition()));
-            OSRMRoadManager roadManager = new OSRMRoadManager(this, "22-Transport");
-            roadManager.setMean(OSRMRoadManager.MEAN_BY_FOOT);
-            Road road = roadManager.getRoad(route);
-//            RoadManager roadManager1 = new GraphHopperRoadManager("9b8e0c01-5851-4b2d-9cc5-184a5a9f40c8", false);
-//            roadManager1.addRequestOption("vehicle=foot");
-//            Road road = roadManager1.getRoad(route);
+//            OSRMRoadManager roadManager = new OSRMRoadManager(this, "22-Transport");
+//            roadManager.setMean(OSRMRoadManager.MEAN_BY_FOOT);
+//            Road road = roadManager.getRoad(route);
+            RoadManager roadManager1 = new GraphHopperRoadManager("9b8e0c01-5851-4b2d-9cc5-184a5a9f40c8", false);
+            roadManager1.addRequestOption("vehicle=foot");
+            Road road = roadManager1.getRoad(route);
             Log.d("Routing", road.mDuration / 60 + "mn | " + road.mLength);
             marker.setSnippet(nomStation + "\n " + " " + nomAr + "\n" + "Duration: " + road.mDuration / 60 + "minutes\nDistance: " + road.mLength + "km");
 //            Toast.makeText(this, "Duration: "+road.mDuration/60+"minutes\nDistance: "+road.mLength+"km", Toast.LENGTH_LONG).show();
             Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
             map1.getOverlays().add(roadOverlay);
             map1.invalidate();
+            myMap.invalidate();
 
 //            Toast.makeText(MainActivity.this,
 //                    marker1.getSnippet() +
@@ -300,102 +421,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    void getLocation() {
-        if (locationManager == null) {
-            locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
 
-            Criteria criteres = new Criteria();
-            criteres.setAccuracy(Criteria.ACCURACY_FINE);
-            criteres.setAltitudeRequired(true);
-            criteres.setBearingRequired(true);
-            criteres.setSpeedRequired(true);
-            criteres.setCostAllowed(true);
-            criteres.setPowerRequirement(Criteria.POWER_MEDIUM);
-            fournisseur = locationManager.getBestProvider(criteres, true);
-        }
-        if (fournisseur != null) {
-            // dernière position connue
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                Log.d("Offline", "no permissions !");
-
-                return;
-            }
-            Location localisation = locationManager.getLastKnownLocation(fournisseur);
-            if (localisation != null) {
-                // on notifie la localisation
-                ecouteurGPS.onLocationChanged(localisation);
-                currentLocation.setLatitude(localisation.getLatitude());
-                currentLocation.setLongitude(localisation.getLongitude());
-            }
-
-            Log.d("Offline", "Marche");
-        } else {
-            Log.d("status", "GPS desactive");
-        }
-    }
-
-    LocationListener ecouteurGPS = new LocationListener() {
-
-        @Override
-        public void onLocationChanged(Location localisation) {
-//            String coordonnees = String.format("Latitude : %f - Longitude : %f\n", localisation.getLatitude(), localisation.getLongitude());
-            myMap.getController().setCenter(new GeoPoint(localisation.getLatitude(), localisation.getLongitude()));
-            myMap.invalidate();
-
-//            List<Address> adresses = null;
-//            try {
-////                adresses = geocoder.getFromLocation(localisation.getLatitude(), localisation.getLongitude(), 1);
-//            } catch (IllegalArgumentException illegalArgumentException) {
-//                Log.e("GPS", "erreur " + coordonnees, illegalArgumentException);
-//            }
-//
-//            if (adresses == null || adresses.size() == 0) {
-//                Log.e("GPS", "erreur aucune adresse !");
-//            } else {
-//                Address adresse = adresses.get(0);
-//                ArrayList<String> addressFragments = new ArrayList<>();
-//
-//                for (int i = 0; i <= adresse.getMaxAddressLineIndex(); i++) {
-//                    addressFragments.add(adresse.getAddressLine(i));
-//                }
-//            }
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(@NonNull String provider) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(@NonNull String provider) {
-
-        }
-    };
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        arreterLocalisation();
-    }
-
-    public void onResume() {
-        super.onResume();
-    }
-
-    public void onPause() {
-        super.onPause();
-    }
-
-    private void arreterLocalisation() {
-        if (locationManager != null) {
-            locationManager.removeUpdates(ecouteurGPS);
-            ecouteurGPS = null;
-        }
-    }
 }
