55642ab45ce9563afccd9aae7329f80ae78af6c4
abdoukuolem96 <abdoukuolem96@gmail.com>
2021-05-12 05:13:17 +0100

    Delete the road when you choose another station

--- app/src/main/java/com/esi/navigator_22/MainActivity.java
+++ app/src/main/java/com/esi/navigator_22/MainActivity.java
@@ -55,12 +55,13 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    //    private static MapView myMap;
     String urlStations = "http://192.168.1.7:3000/stations";
     String urlChemin = "http://192.168.1.7:3000/polyline";
     private String myResponse;
+    int numberOfOverlays = 1;
 
     LocationManager locationManager;
-    private String fournisseur;
     private MapView myMap;
     ScaleBarOverlay echelle;
     MyLocationNewOverlay mLocationOverlay;
@@ -77,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<Station> stations = new ArrayList<>();
     ArrayList<GeoPoint> chemin = new ArrayList<>();
+    ArrayList<Polyline> roads = new ArrayList<>();
     public ArrayList<GeoPoint> history = new ArrayList<>();
     int minZ = 2;
     int maxZ = 17;
@@ -111,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d("LogGps", "Permissions granted");
         } else {
             // You can directly ask for the permission.
-            Toast.makeText(this, "Localisation requise", Toast.LENGTH_LONG);
+            Toast.makeText(this, "Localisation requise", Toast.LENGTH_LONG).show();
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1
             );
 //            getLocation();
@@ -128,20 +130,24 @@ public class MainActivity extends AppCompatActivity {
         mLocationOverlay.setDirectionArrow(currentIcon, currentIcon);
         mLocationOverlay.enableMyLocation();
         mLocationOverlay.getMyLocation();
+        myMap.getOverlays().add(mLocationOverlay);
+        numberOfOverlays++;
 //        history.add(mLocationOverlay.getMyLocation());
 
 
         echelle = new ScaleBarOverlay(this.myMap);
         myMap.getOverlays().add(echelle);
+        numberOfOverlays++;
         myMap.setMultiTouchControls(true);
         RotationGestureOverlay mRotationGestureOverlay = new RotationGestureOverlay(this.myMap);
         mRotationGestureOverlay.setEnabled(true);
         myMap.setMultiTouchControls(true);
         myMap.getOverlays().add(mRotationGestureOverlay);
-        myMap.getOverlays().add(mLocationOverlay);
+        numberOfOverlays++;
 
-        currentPosition.setOnClickListener(v -> {
-//            currentLocation = history.get(history.size()-1);
+
+//        currentPosition.setOnClickListener(v -> {
+////            currentLocation = history.get(history.size()-1);
 //            locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
 //            Criteria criteres = new Criteria();
 //            criteres.setAccuracy(Criteria.ACCURACY_FINE);
@@ -150,19 +156,28 @@ public class MainActivity extends AppCompatActivity {
 //            criteres.setSpeedRequired(true);
 //            criteres.setCostAllowed(true);
 //            criteres.setPowerRequirement(Criteria.POWER_MEDIUM);
-//            criteres.setPowerRequirement(Criteria.POWER_MEDIUM);
 //            fournisseur = locationManager.getBestProvider(criteres, true);
-//            Location l = new Location(GPS_PROVIDER);
+////            Location l = new Location(GPS_PROVIDER);
 ////            ecouteurGPS.onLocationChanged(locationManager.getLastKnownLocation(fournisseur));
-//            ecouteurGPS.onLocationChanged(locationManager.getLastKnownLocation(GPS_PROVIDER));
-//            currentLocation = history.get(history.size());
+////            ecouteurGPS.onLocationChanged(locationManager.getLastKnownLocation(GPS_PROVIDER));
+////            currentLocation = history.get(history.size());
+//            currentLocation = mLocationOverlay.getMyLocation();
+//            if (currentLocation != null) {
+//                Log.d("LogGps1", "Button clicked and current location = " + currentLocation.toString());
+//            } else {
+//                currentLocation.setLatitude(locationManager.getLastKnownLocation(fournisseur).getLatitude());
+//                currentLocation.setLongitude(locationManager.getLastKnownLocation(fournisseur).getLongitude());
+//                Log.d("LogGps","Using the provider : "+fournisseur);
+//                Log.d("LogGps2", "Button clicked and current location = " + currentLocation.toString());
+//            }
+//            myMap.getController().setCenter(currentLocation);
+//            myMap.getController().setZoom(16.0);
+//        });
 
-            currentLocation = mLocationOverlay.getMyLocation();
-            Log.d("LogGps", "Button clicked and history = " + history.size());
-            myMap.getController().setCenter(currentLocation);
-            myMap.getController().setZoom(16.0);
-            Log.d("LogGps", "Button clicked and current location = " + currentLocation.toString());
+        currentPosition.setOnClickListener(v -> {
+
         });
+
 
         Request request = new Request.Builder()
                 .url(urlChemin)
@@ -202,58 +217,65 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        stations = database.getAllStations();
-        Log.d("LogDatabase", String.valueOf(stations.size()));
-        for (int i = 0; i < stations.size(); i++) {
-            addMarker(this, myMap, stations.get(i).coordonnees, stations.get(i).nomFr, stations.get(i).nomAr);
-        }
-
         chemin = database.getAllPointsChemin();
         Log.d("LogDatabase", String.valueOf(chemin.size()));
         tracerChemin(chemin, myMap);
 
+        stations = database.getAllStations();
+        Log.d("LogDatabase", String.valueOf(stations.size()));
+        for (int i = 0; i < stations.size(); i++) {
+            addMarker(this, myMap, stations.get(i).coordonnees, stations.get(i).nomFr, stations.get(i).nomAr);
+            numberOfOverlays++;
+        }
+
 
         Log.d("LogDatabase", database.getAllPointsChemin().toString());
 
+    }
+
+    void getLocation() {
 
     }
 
     LocationListener ecouteurGPS = new LocationListener() {
 
 
-        @Override
-        public void onLocationChanged(Location localisation) {
-//            String coordonnees = String.format("Latitude : %f - Longitude : %f\n", localisation.getLatitude(), localisation.getLongitude());
-            GeoPoint current = new GeoPoint(0.0, 0.0);
-//            current = new MyLocationNewOverlay(new GpsMyLocationProvider(getApplicationContext()), myMap).getMyLocation();
-            current = mLocationOverlay.getMyLocation();
-            if (localisation != null) {
-                history.add(new GeoPoint(localisation.getLatitude(), localisation.getLongitude()));
-            }
-            Log.d("LogGps", "Marche et currentlocation1 = " + String.valueOf(history.size()));
-            Log.d("LogGps", "Marche et currentlocation2 = " + String.valueOf(current));
-            myMap.getController().setCenter(current);
-            myMap.getController().setZoom(16.0);
-            Log.d("LogGps", "onLocationChanged-252");
-            myMap.invalidate();
+//        @Override
+//        public void onLocationChanged(Location localisation) {
+//            GeoPoint current = new GeoPoint(0.0, 0.0);
+////            current = mLocationOverlay.getMyLocation();
+//            current.setLatitude(localisation.getLatitude());
+//            current.setLongitude(localisation.getLongitude());
+//            Log.d("LogGps", "Marche et currentlocation2 = " + String.valueOf(current));
+//            myMap.getController().setCenter(current);
+//            myMap.getController().setZoom(16.0);
+//            Log.d("LogGps", "onLocationChanged-252");
+//            myMap.invalidate();
+//
+////            List<Address> adresses = null;
+////            try {
+//////                adresses = geocoder.getFromLocation(localisation.getLatitude(), localisation.getLongitude(), 1);
+////            } catch (IllegalArgumentException illegalArgumentException) {
+////                Log.e("GpsLog", "erreur " + coordonnees, illegalArgumentException);
+////            }
+////
+////            if (adresses == null || adresses.size() == 0) {
+////                Log.e("GpsLog", "erreur aucune adresse !");
+////            } else {
+////                Address adresse = adresses.get(0);
+////                ArrayList<String> addressFragments = new ArrayList<>();
+////
+////                for (int i = 0; i <= adresse.getMaxAddressLineIndex(); i++) {
+////                    addressFragments.add(adresse.getAddressLine(i));
+////                }
+////            }
+//        }
 
-//            List<Address> adresses = null;
-//            try {
-////                adresses = geocoder.getFromLocation(localisation.getLatitude(), localisation.getLongitude(), 1);
-//            } catch (IllegalArgumentException illegalArgumentException) {
-//                Log.e("GpsLog", "erreur " + coordonnees, illegalArgumentException);
-//            }
-//
-//            if (adresses == null || adresses.size() == 0) {
-//                Log.e("GpsLog", "erreur aucune adresse !");
-//            } else {
-//                Address adresse = adresses.get(0);
-//                ArrayList<String> addressFragments = new ArrayList<>();
-//
-//                for (int i = 0; i <= adresse.getMaxAddressLineIndex(); i++) {
-//                    addressFragments.add(adresse.getAddressLine(i));
-//                }
-//            }
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            currentLocation = new GeoPoint(location);
+            getLocation();
         }
 
         @Override
@@ -374,51 +396,59 @@ public class MainActivity extends AppCompatActivity {
         line.setPoints(chemin);
         line.setGeodesic(true);
         mapView.getOverlayManager().add(line);
+
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
-    public void addMarker(Context context, MapView map, GeoPoint position, String nomStation, String nomAr) {
+    public void addMarker(Context context, MapView mapMarker, GeoPoint positionMarker, String nomFrMarker, String nomArMarker) {
         float[] distance = new float[1];
-        Location.distanceBetween(currentLocation.getLatitude(), currentLocation.getLongitude(), position.getLatitude(), position.getLongitude(), distance);
-        Marker marker = new Marker(map);
-        marker.setPosition(position);
+        Location.distanceBetween(currentLocation.getLatitude(), currentLocation.getLongitude(), positionMarker.getLatitude(), positionMarker.getLongitude(), distance);
+        Marker marker = new Marker(mapMarker);
+        marker.setPosition(positionMarker);
         marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         marker.setAlpha(1);
         marker.setIcon(context.getResources().getDrawable(R.drawable.ic_tramway));
-        marker.setSnippet(nomStation + "\n " + " " + nomAr);
+//        marker.setSnippet(nomFrMarker + "\n " + " " + nomArMarker);
+        marker.setTitle(nomFrMarker + " " + nomArMarker);
         marker.setPanToView(true);
-        map.invalidate();
-        map.getOverlays().add(marker);
+        mapMarker.invalidate();
+        mapMarker.getOverlays().add(marker);
 
-        marker.setOnMarkerClickListener((marker1, map1) -> {
-            map1.invalidate();
-            ArrayList<GeoPoint> route = new ArrayList<>();
-            route.add(mLocationOverlay.getMyLocation());
-            route.add(new GeoPoint(marker1.getPosition()));
+        marker.setOnMarkerClickListener((marker1, mapView) -> {
+            if (mapView.getOverlays().size() > numberOfOverlays) {
+                mapView.getOverlays().remove(mapView.getOverlays().get(numberOfOverlays));
+            }
+            ArrayList<GeoPoint> roadPoints = new ArrayList<>();
+            roadPoints.add(marker1.getPosition());
+            roadPoints.add(mLocationOverlay.getMyLocation());
+            OSRMRoadManager roadManager = new OSRMRoadManager(getApplicationContext(), "22-Transport");
+            roadManager.setMean(OSRMRoadManager.MEAN_BY_FOOT);
+            Road road = roadManager.getRoad(roadPoints);
+            Polyline route = RoadManager.buildRoadOverlay(road);
 //            OSRMRoadManager roadManager = new OSRMRoadManager(this, "22-Transport");
 //            roadManager.setMean(OSRMRoadManager.MEAN_BY_FOOT);
 //            Road road = roadManager.getRoad(route);
-            RoadManager roadManager1 = new GraphHopperRoadManager("9b8e0c01-5851-4b2d-9cc5-184a5a9f40c8", false);
-            roadManager1.addRequestOption("vehicle=foot");
-            Road road = roadManager1.getRoad(route);
-            Log.d("Routing", road.mDuration / 60 + "mn | " + road.mLength);
-            marker.setSnippet(nomStation + "\n " + " " + nomAr + "\n" + "Duration: " + road.mDuration / 60 + "minutes\nDistance: " + road.mLength + "km");
-//            Toast.makeText(this, "Duration: "+road.mDuration/60+"minutes\nDistance: "+road.mLength+"km", Toast.LENGTH_LONG).show();
-            Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
-            map1.getOverlays().add(roadOverlay);
-            map1.invalidate();
-            myMap.invalidate();
+//            RoadManager roadManager1 = new GraphHopperRoadManager("9b8e0c01-5851-4b2d-9cc5-184a5a9f40c8", false);
+//            roadManager1.addRequestOption("vehicle=foot");
+//            Road road = roadManager1.getRoad(route);
 
-//            Toast.makeText(MainActivity.this,
-//                    marker1.getSnippet() +
-//                            "\n" + String.valueOf(distance[0]) + "m",
-//                    Toast.LENGTH_SHORT).show();
-            marker.showInfoWindow();
-            map1.getController().setCenter(marker1.getPosition());
-            map1.getController().setZoom(16);
-            map1.invalidate();
-            return false;
+            roads.add(route);
+            mapView.getOverlays().add(roads.get(roads.size() - 1));
+            marker1.setSnippet("Temps restant pour arriver الوقت اللازم للوصول دقيقة" + road.mDuration / 60 + " minutes");
+            marker1.showInfoWindow();
+            mapView.getController().setCenter(marker1.getPosition());
+            mapView.getController().setZoom(16.0);
+            return true;
         });
+
+//        marker.setOnMarkerClickListener((marker1, map1) -> {
+
+//            OSRMRoadManager roadManager = new OSRMRoadManager(this, "22-Transport");
+//            roadManager.setMean(OSRMRoadManager.MEAN_BY_FOOT);
+//            Road road = roadManager.getRoad(route);
+//            RoadManager roadManager1 = new GraphHopperRoadManager("9b8e0c01-5851-4b2d-9cc5-184a5a9f40c8", false);
+//            roadManager1.addRequestOption("vehicle=foot");
+//            Road road = roadManager1.getRoad(route);
     }
 
 
